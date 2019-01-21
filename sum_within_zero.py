192222eff893f8485cd191bc2a6300cc3a622db6
vitcmaestro <40243064+vitcmaestro@users.noreply.github.com>
2019-01-21 19:44:10 +0530

    Update sum_within_zero.py

--- sum_within_zero.py
+++ sum_within_zero.py
@@ -1,6 +1,16 @@
 n = int(input(""))
 lis = list(map(int,input().split()))
+miner = 100
+c =0
 for i in range(n):
     for j in range(i+1,n):
         if(lis[i]+lis[j] == 0):
-            print(lis[i],lis[j])
+            print(lis[i]+lis[j])
+            c+=1
+        else:
+            if(lis[i]+lis[j] < miner):
+                miner = lis[i]+lis[j]
+                a = lis[i] 
+                b = lis[j]
+if(c ==0):
+    print(a,b)
