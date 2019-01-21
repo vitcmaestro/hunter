9b0e1128cefdef871dd2613a690853da69849023
vitcmaestro <40243064+vitcmaestro@users.noreply.github.com>
2019-01-21 16:18:38 +0530

    Create most_repeatin_first_ele.py

---
+++ most_repeatin_first_ele.py
@@ -0,0 +1,15 @@
+def frequency(i):
+    c=0
+    for j in lis:
+        if(j == i):
+            c+=1
+    if(c>maxer):
+        return c,i
+    else:
+        return maxer,ans
+n = int(input(""))
+lis = list(map(int,input().split()))
+maxer = 0
+for i in lis:
+    maxer,ans = frequency(i)
+print(ans)
