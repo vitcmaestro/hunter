dc4ee7baf17edcad2388c2abd011ae180d7fdfd8
vitcmaestro <40243064+vitcmaestro@users.noreply.github.com>
2019-01-21 22:06:06 +0530

    Create permutations.py

---
+++ permutations.py
@@ -0,0 +1,18 @@
+def permutation(lst): 
+    if len(lst) == 0: 
+        return [] 
+    if len(lst) == 1: 
+        return [lst] 
+    l = [] 
+    for i in range(len(lst)): 
+       m = lst[i] 
+       remLst = lst[:i] + lst[i+1:] 
+       for p in permutation(remLst): 
+           l.append([m] + p) 
+    return l 
+   
+data = list(input("")) 
+for p in permutation(data): 
+    for i in p:
+        print(i,end="")
+    print()
