4a07f83c083c1685612e9abc039b0d20d47f5ed3
vitcmaestro <40243064+vitcmaestro@users.noreply.github.com>
2019-01-21 19:22:22 +0530

    Update even_odd_num_or_ind.py

--- even_odd_num_or_ind.py
+++ even_odd_num_or_ind.py
@@ -7,7 +7,10 @@ for i in range(n):
             print(lis[i],end="")
             c+=1
         else:
-            print("",lis[i],end="")
+            if(lis[i] == 1 and lis[i-1] == 2 and lis[i-1] == 1):
+                print("",lis[i],end=" ")
+            else:
+                print("",lis[i],end="")
     elif(lis[i]%2 == 0 and i%2 !=0):
         if(c == 0):
             print(lis[i],end="")
