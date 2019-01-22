9bada1079c193ded72b1fe07d694cedbd13e753b
vitcmaestro <40243064+vitcmaestro@users.noreply.github.com>
2019-01-22 16:28:01 +0530

    Create num_hunter_matrix.py

---
+++ num_hunter_matrix.py
@@ -0,0 +1,19 @@
+n,k = map(int,input().split())
+lis =[]
+ans=[]
+for i in range(n):
+    temp = list(map(int,input().split()))
+    lis.append(temp)
+for j in range(k):
+    c=0
+    for i in range(1,n):
+        for k in range(k):
+            if(lis[0][j] == lis[i][k]):
+                c+=1
+    if(c == n-1):
+        ans.append(lis[0][j])
+for i in range(len(ans)):
+    if(i==0):
+        print(ans[i],end="")
+    else:
+        print("",ans[i],end="")
