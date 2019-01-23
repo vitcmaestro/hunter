4fc22018a4cda0edf9a2c436481998eff1139523
vitcmaestro <40243064+vitcmaestro@users.noreply.github.com>
2019-01-23 18:52:54 +0530

    Update kolappam.py

--- kolappam.py
+++ kolappam.py
@@ -1,20 +1,20 @@
+def changezero(x,y):
+    for k in range(n):
+        for l in range(m):
+            if((k == x or l == y) and nodes[k][l]!=0):
+                nodes[k][l] = 0
 n,m = map(int,input().split())
 node= []
 ind =[]
-temper = [1]*m
-nodes = [temper]*n
+
+nodes = [[1]*m for z in range(n)]
 for i in range(n):
     temp = list(map(int,input().split()))
     node.append(temp)
 for i in range(n):
     for j in range(m):
         if(node[i][j] == 0):
-            ind.append([i,j])
-for ro,co in ind:
-    for i in range(m):
-        nodes[ro][i] = 0
-    for j in range(n):
-        nodes[j][co] = 0
+            changezero(i,j)
 for i in range(n):
     for j in range(m):
         if(j == m-1):
