5444d7b30cddeffe15109c99039563208e2efc37
vitcmaestro <40243064+vitcmaestro@users.noreply.github.com>
2019-01-25 21:10:56 +0530

    Update sub_non_pal.py

--- sub_non_pal.py
+++ sub_non_pal.py
@@ -1,9 +1,10 @@
 s = input("")
-j = -1
+j = len(s)
 for i in range(len(s)-1):
     temp = s[:j]
     test = s[j-1::-1]
     j-=1
+    print(temp,test)
     if(temp != test):
         print(temp)
         exit()
