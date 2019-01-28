21f82cf571ec20a483d9c98e63e121972d2b9160
vitcmaestro <40243064+vitcmaestro@users.noreply.github.com>
2019-01-28 16:08:25 +0530

    Update prod_adj_subarray.py

--- prod_adj_subarray.py
+++ prod_adj_subarray.py
@@ -3,7 +3,13 @@ a = list(map(int,input().split()))
 res =[]
 for i in range(n):
     prod = 1
-    for j in range(i):
+    for j in range(i,n):
+        prod = prod * a[j]
+    res.append(prod)
+
+for i in range(n):
+    prod = 1
+    for j in range(i+1):
         prod = prod * a[j]
     res.append(prod)
 print(max(res))
