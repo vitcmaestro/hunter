0cdd9483e120d165b388976167524a61c8d7bef7
vitcmaestro <40243064+vitcmaestro@users.noreply.github.com>
2019-01-28 21:24:14 +0530

    Create palin_del_one_char.py

---
+++ palin_del_one_char.py
@@ -0,0 +1,14 @@
+s = input()
+s1 = s.lower()
+lis= [0]*26
+for i in s1:
+    x = ord(i)-97
+    lis[x]+=1
+c = 0
+for i in lis:
+    if(lis[i]%2 !=0):
+        c = c+1
+if(c == 1 or c== 0):
+    print("YES")
+else:
+    print("NO")
