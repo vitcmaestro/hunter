056ddc579f7f49a6f247029e75d95b3de1cb7d3c
vitcmaestro <40243064+vitcmaestro@users.noreply.github.com>
2019-01-28 17:33:46 +0530

    Update greatest_num_from_digit.py

--- greatest_num_from_digit.py
+++ greatest_num_from_digit.py
@@ -1,15 +1,15 @@
 s = int(input(""))
 n = list(str(s))
-res =[]
+ress =[]
 for i in range(len(n)):
     for j in range(len(n)-1):
         x =""
         n[j],n[j+1] = n[j+1],n[j]
         for z in n:
             x = x+z
-        if(int(x) not in res):
-            res.append(int(x))
-res.sort()
+        if(int(x) not in ress):
+            ress.append(int(x))
+ress.sort()
 for i in res:
     if(i>s):
         print(i)
