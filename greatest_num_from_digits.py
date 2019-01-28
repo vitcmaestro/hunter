09aa60e810a379f398be09b601c1697045a0a1c9
vitcmaestro <40243064+vitcmaestro@users.noreply.github.com>
2019-01-28 17:28:46 +0530

    Update greatest_num_from_digits.py

--- greatest_num_from_digits.py
+++ greatest_num_from_digits.py
@@ -1,5 +1,19 @@
-n = int(input(""))
-lis = list(map(int,input().split()))
-lis.sort(reverse = True)
-for i in lis:
-    print(i,end = "")
+s = int(input(""))
+n = list(str(s))
+res =[]
+for i in range(len(n)):
+    for j in range(len(n)-1):
+        x =""
+        n[j],n[j+1] = n[j+1],n[j]
+        for z in n:
+            x = x+z
+        if(int(x) not in res):
+            res.append(int(x))
+res.sort()
+for i in res:
+    if(i>s):
+        print(i)
+        break
+else:
+    print("impossible")
+
